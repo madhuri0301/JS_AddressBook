5ba31bfb754feab80d8af0e977af4bfeeb21e6fb
Madhuri <harshupatil0301@gmail>
2021-07-15 21:13:21 +0530

    [Madhuri]Refactor : Calculating Number Of AddressBook Present

--- addressBook.js
+++ addressBook.js
@@ -82,7 +82,7 @@ let flag = true;
 var addressBookList = []
 
 while(flag == true){
-    const option = Number(readline.question("Chosse Your option: \n1.For add new Contact. \n2.Edit Contact Using Name. \n3. Delete Contact Using Name \nAny Number To Exit" ))
+    const option = Number(readline.question("Chosse Your option: \n1.For add new Contact. \n2.Edit Contact Using Name. \n3. Delete Contact Using Name \n4. For Get Number Of Contact In Book \nAny Number To Exit"))
     switch(option){
         case 1:
             addEntries(addressBookList);
@@ -92,6 +92,9 @@ while(flag == true){
             break;
         case 3:
             deleteEntries(addressBookList);
+            break;
+        case 4:
+            sizeOfBook(addressBookList);
             break;
         default:
             flag = false;
@@ -150,4 +153,10 @@ function deleteEntries(addressBookList){
     });
     console.log(addressBookList.toString());
 }
+function sizeOfBook(addressBookList){
+    console.log(`${addressBookList.length} Entries In Our AddressBook`);
+}
+
+
+
 
